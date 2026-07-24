fcfa9f7344acb0766ac91a0ac7ddaef37de44848
daliaGaber <daliagaber2002bfcai@gmail.com>
2026-07-24 23:41:24 +0300

    Task 1

--- lib/task1.dart
+++ lib/task1.dart
@@ -1,3 +1,17 @@
-void main(){
-  
+void main() {
+  const String schoolName = 'Future School';
+  final DateTime registrationDate = DateTime.now();
+  int studentId = 1024;
+  String studentName = 'Ahmed';
+  double gpa = 3.85;
+  String? email; 
+  String? phoneNumber = '0501234567';
+  email ??= "ahmed@email.com"; 
+
+  print('School: $schoolName');
+  print('Date: ${registrationDate.day}/${registrationDate.month}/${registrationDate.year}');
+  print('Student id: $studentId');
+  print('Student name: $studentName');
+  print('Student GPA: $gpa');
+  print('Email: $email');
 }
